5f0cf6bd2d6472a82db8c1d95a8a36c4845c6d9e
Kazuya-san <kazuyasan3286@gmail.com>
2023-02-08 15:38:33 +0500

    new changes with custom server3

--- server.js
+++ server.js
@@ -1,36 +1,123 @@
-const { createServer } = require("http");
-const { parse } = require("url");
+const express = require("express");
 const next = require("next");
+const { PrismaClient } = require("@prisma/client");
+// import express from "express";
+// import next from "next";
+// import { PrismaClient } from "@prisma/client";
 
-const dev = process.env.NODE_ENV !== "production";
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
-// when using middleware `hostname` and `port` must be provided below
-const hostname = process.env.CLIENT_URL || "localhost";
-const app = next({ dev, hostname, port });
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
-
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
+  const server = express();
 
-      if (pathname === "/a") {
-        await app.render(req, res, "/a", query);
-      } else if (pathname === "/b") {
-        await app.render(req, res, "/b", query);
-      } else {
-        await handle(req, res, parsedUrl);
-      }
-    } catch (err) {
-      console.error("Error occurred handling", req.url, err);
-      res.statusCode = 500;
-      res.end("internal server error");
+  //handle a request that is /:id and return me the id
+
+  server.get("/:id", async (req, res) => {
+    const slug = req.params.id;
+
+    console.log(slug);
+
+    if (!slug) {
+      res.statusCode = 404;
+
+      res.json({ message: "pls use with a slug" });
+
+      return;
     }
-  }).listen(port, (err) => {
+
+    const data = await prisma.url.findFirst({
+      where: {
+        slug: {
+          equals: slug,
+        },
+      },
+    });
+
+    if (!data) {
+      res.statusCode = 404;
+
+      res.send(JSON.stringify({ message: "slug not found" }));
+
+      return;
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    //increment the count
+    await prisma.url.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        clicks: {
+          increment: 1,
+        },
+      },
+    });
+
+    console.log(data.url);
+
+    res.redirect(data.url);
+
+    //return res.json(data);
+  });
+
+  server.get("/api/auth", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get("/api/auth/callback", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get("/api/create-api-key", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.post("/api/create-api-key", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.put("/api/create-api-key", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.delete("/api/create-api-key", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get("/api/create-url", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.post("/api/create-url", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.put("/api/create-url", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.delete("/api/create-url", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get("/api/get-url", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get("/api/get-urls", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get("*/*", (req, res) => {
+    return handle(req, res);
+  });
+  server.listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
