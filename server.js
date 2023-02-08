c5cadfaf5589e930e2634aac438139143ece2f09
Kazuya-san <kazuyasan3286@gmail.com>
2023-02-08 13:55:35 +0500

    new changes with custom server2

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
+const hostname = process.env.CLIENT_URL || "localhost";
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
